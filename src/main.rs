a0166959e4afbfe5c0b517825b750bc91edaba60
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-05-04 10:07:59 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
-#![feature(proc_macro_hygiene, decl_macro)] 
-
-use rocket;
-use rocket_contrib;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 
 mod models;
 mod routes;
 mod handlers;
 
-use rocket_contrib::databases::postgres;
-use rocket_contrib::templates::tera::Template;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
 
-#[database("notes")]
-pub struct DbConn(postgres::Connection);
+    HttpServer::new(|| {
+        let notes_db = models::NotesDb::new();
 
-fn main() {
-    rocket::ignite()
-        .attach(DbConn::fairing())
-        .mount("/", routes![
-            routes::index,
-            routes::create_note,
-            routes::read_note,
-            routes::edit_note,
-            routes::update_note,
-            routes::delete_note,
-            routes::search_notes
-        ])
-        .attach(Template::fairing())
-        .launch();
+        App::new()
+            .data(notes_db)
+            .route("/", web::get().to(routes::index))
+            .route("/note", web::post().to(routes::create_note))
+            .route("/note/{id}", web::get().to(routes::read_note))
+            .route("/note/{id}", web::put().to(routes::update_note))
+            .route("/note/{id}", web::delete().to(routes::delete_note))
+            .route("/notes", web::get().to(routes::search_notes))
+            .wrap(Logger::default())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
